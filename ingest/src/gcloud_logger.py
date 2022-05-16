3573b826fc80bf924212638a901911876f0f0f85
Adrian Mroz <theadriannno@gmail.com>
2022-05-16 19:02:46 +0200

    added logger and config file for data ingestion

---
+++ ingest/src/gcloud_logger.py
@@ -0,0 +1,21 @@
+import google.cloud.logging
+import logging
+
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+
+def gcloud_logger(name):
+    # Instantiate Google Cloud logger client
+    gcloud_log_client = google.cloud.logging.Client()
+
+    # Create custom handler
+    handler = CloudLoggingHandler(gcloud_log_client)
+    formatter = logging.Formatter(" %(levelname)s : %(message)s")
+    handler.setFormatter(formatter)
+    
+    # Create logging handlers using standard library (by default will be sent to Logging)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
